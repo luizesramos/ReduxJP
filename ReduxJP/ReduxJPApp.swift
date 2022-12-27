db247fe3b9fab60520a6f1c9299efd2df71f0b93
Luiz Ramos <lramos@pandora.com>
2022-12-27 18:43:16 -0500

    Initial Commit

---
+++ ReduxJP/ReduxJPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxJPApp.swift
+//  ReduxJP
+//
+//  Created by Luiz Ramos on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct ReduxJPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
